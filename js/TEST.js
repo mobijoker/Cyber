51b73145ada5256d12b95f05e70f1dca39404861
Arthur Gareginyan <arthurgareginyan@gmail.com>
2017-04-15 00:36:16 +0300

    Update

--- js/TEST.js
+++ js/TEST.js
@@ -3,4 +3,7 @@
 
     var language = ( navigator.language || navigator.browserLanguage ).split('-')[0];
 
+    //alert(language);
+    //document.getElementsByClassName("test-lang")[0].innerHTML=language;
+
 })();
