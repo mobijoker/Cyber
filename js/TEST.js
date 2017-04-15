f33eb545f61ac63f1896056a82dfe8373b7d8cc9
Arthur Gareginyan <arthurgareginyan@gmail.com>
2017-04-15 03:55:59 +0300

    Update

--- js/TEST.js
+++ js/TEST.js
@@ -1,9 +1,25 @@
 
 (function() {
 
-    var language = ( navigator.language || navigator.browserLanguage ).split('-')[0];
+    //var language = ( navigator.language || navigator.browserLanguage ).split('-')[0];
 
     //alert(language);
-    //document.getElementsByClassName("test-lang")[0].innerHTML=language;
+    //document.getElementsByClassName("test-lang")[0].innerHTML = "language";
+    //document.getElementById("test-lang").innerHTML = "language";
 
 })();
+
+jQuery(document).ready(function($) {
+
+    "use strict";
+
+    //var language = ( navigator.language || navigator.browserLanguage ).split('-')[0];
+
+    //alert(language);
+
+    //
+    //if (".test-lang") {
+    //    $( ".test-lang" ).html( "<span class='red'>Hello <b>Again</b></span>" );
+    //}
+
+});
